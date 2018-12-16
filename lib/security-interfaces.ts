8d022b2e481686af3d408bdecdee44e6d786862f
DrFriendless <friendless.farrell@gmail.com>
2018-12-16 20:07:34 +1100

    v0.12.0 adding features to UserData

--- lib/security-interfaces.ts
+++ lib/security-interfaces.ts
@@ -2,19 +2,38 @@ export interface Identity {
   jwt: string;
 }
 
+export class BuddySet {
+    private readonly name: string;
+    private readonly buddies: string[];
+
+    constructor(name: string, buddies: string[]) {
+        this.name = name;
+        this.buddies = buddies;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public getBuddies(): string[] {
+        return this.buddies;
+    }
+}
+
 export interface UserData {
   username: string | undefined;
   first: boolean;
+  buddies: BuddySet[];
   config: UserConfig;
   jwt: Decoded;
 }
 
+// this is everything that we store.
 export interface PersonalData {
   userData: UserData | undefined;
   allData: object | undefined;
   error: string | undefined;
 }
-
 
 export interface Decoded {
   nickname: string;
