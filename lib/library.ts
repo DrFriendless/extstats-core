83bac42e4b2ba0765ce6c31c283049abed44a7ba
DrFriendless <Borod1n>
2019-10-26 11:58:44 +1100

    added generic makeIndex method

--- lib/library.ts
+++ lib/library.ts
@@ -7,6 +7,8 @@ export function roundRating(r: number): number {
     return rating;
 }
 
+type HasBGGID = { bggid: number };
+
 export function makeGamesIndex(games: GameData[]): { [bggid: number]: GameData } {
     const result = {} as  { [bggid: number]: GameData };
     if (games) games.forEach(gd => result[gd.bggid] = gd);
@@ -16,5 +18,11 @@ export function makeGamesIndex(games: GameData[]): { [bggid: number]: GameData }
 export function makePlaysIndex(plays: GamePlays[]): { [bggid: number]: GamePlays } {
     const result: { [bggid: number]: GamePlays } = {};
     if (plays) plays.forEach(gp => result[gp.game] = gp);
+    return result;
+}
+
+export function makeIndex<C extends HasBGGID>(games: C[]): Record<string, C> {
+    const result: Record<string, C> = {};
+    if (games) games.forEach(gd => result[gd.bggid] = gd);
     return result;
 }
