7c041af0b52ccec6f55afcd447cba7455df18efc
DrFriendless <Borod1n>
2020-06-08 15:52:00 +1000

    v 1.10.0 - added smarts to ExpansionData; need to add unit testing to this package!

--- lib/expansion-data.ts
+++ lib/expansion-data.ts
@@ -16,23 +16,49 @@ export class ExpansionData {
         this.expansionToBaseGames = mapValues(groupBy(rows, (row: ExpansionRow) => row.expansion), (rs: ExpansionRow[]) => rs.map(row => row.basegame));
     }
 
-    public isExpansion(game: number): boolean {
+    isExpansion(game: number): boolean {
         return !!this.expansionToBaseGames[game];
     }
 
-    public isBasegameOf(maybeBaseGame: number, maybeExpansion: number): boolean {
+    isBasegameOf(maybeBaseGame: number, maybeExpansion: number): boolean {
         const exps = this.gameToExpansions[maybeBaseGame] as number[];
         return exps && exps.indexOf(maybeExpansion) >= 0;
     }
 
-    public isAnyBasegameOf(maybeBaseGames: number[], maybeExpansion: number): boolean {
+    isAnyBasegameOf(maybeBaseGames: number[], maybeExpansion: number): boolean {
         const basegames = this.expansionToBaseGames[maybeExpansion] as number[];
         return basegames && some(maybeBaseGames, (mbg: number) => basegames.indexOf(mbg) >= 0);
     }
 
-    public getUniqueBasegame(expansion: number): number | undefined {
+    getUniqueBasegame(expansion: number): number | undefined {
         const basegames = this.expansionToBaseGames[expansion];
         if (basegames && basegames.length == 1) return basegames[0];
         return undefined;
+    }
+
+    getBasegames(expansion: number): number[] {
+        return this.expansionToBaseGames[expansion] || [];
+    }
+
+    getPossibleBasegameChains(expansion: number, inProgress: number[] = []): number[][] {
+        if (!this.isExpansion(expansion)) return [[]];
+        // need to avoid loops here.
+        if (inProgress.indexOf(expansion) >= 0) return [];
+        inProgress.push(expansion);
+        const result: number[][] = [];
+        const bgs = this.getBasegames(expansion);
+        for (const bg of bgs) {
+            const chains = this.getPossibleBasegameChains(bg, inProgress);
+            for (const chain of chains) {
+                chain.push(bg);
+                result.push(chain);
+            }
+        }
+        if (bgs.length === 0) {
+            // we don't know the base game because it's not in our data
+            return [[expansion]];
+        }
+        inProgress.pop();
+        return result;
     }
 }
